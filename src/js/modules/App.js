5b2cd60943b904264d6ac97f46e70ff6430db4dc
himani-webonise <himani.sharma@weboniselab.com>
2017-10-09 17:32:58 +0530

    Webpack integration in react project

--- src/js/modules/App.js
+++ src/js/modules/App.js
@@ -1,17 +1,16 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
 
+console.log('app.js')
 class App extends Component {
+  debugger
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          To get started, edit Test and save to reload.
         </p>
       </div>
     );
